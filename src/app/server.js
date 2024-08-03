864241679da70c7876103b8ad84bed3ed9404588
DevoneJ <devone@whatscookin.com>
2024-08-03 16:31:50 -0400

    This version of the server uses json information about a tournament to display information. It has information about the name, org, game, and dates. It has placeholder links in the served html for when we get the other webpages up and running. To look at a specific tournament you give it the route "tournament/" followed by a tournament id number added controllers from main

--- src/app/server.js
+++ src/app/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-
-let hostname = "0.0.0.0"
+let hostname = "0.0.0.0";
 let port = 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
 
 app.use('/api', require('./routes'));
+
 
 app.listen(port, hostname, () => {
     console.log(`http://${hostname}:${port}`);
