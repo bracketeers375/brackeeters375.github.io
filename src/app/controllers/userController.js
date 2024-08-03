8b43fb457d4852379b2b5f4c5399ac0e2158e0fd
Steven <sd3522@drexel.edu>
2024-08-03 18:17:43 -0400

    core logic of user registration page

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,19 +1,68 @@
+const env = require("../env.json");
+const pg = require("pg");
+const Pool = pg.Pool;
+const pool = new Pool(env);
+
 exports.getUserById = async (req, res) => {
-    const { id } = req.params;
+    const {id} = req.params;
     res.send(`User with ID: ${id} got`);
 };
 
 exports.createUser = async (req, res) => {
-    const { id } = req.params;
-    res.send(`User with ID: ${id} created`);
+    let body = req.body;
+    let username;
+    let email;
+    let cemail;
+    let password;
+    let cpassword;
+
+    if (!body.hasOwnProperty("username") ||
+        !body.hasOwnProperty("email") ||
+        !body.hasOwnProperty("cemail") ||
+        !body.hasOwnProperty("password") ||
+        !body.hasOwnProperty("cpassword")
+    ) {
+        res.status(400);
+        return res.send();
+    }
+
+    username = body.username;
+    email = body.email;
+    cemail = body.cemail;
+    password = body.password;
+    cpassword = body.cpassword;
+
+    if (email.toLowerCase() !== cemail.toLowerCase()) {
+        res.status(400);
+        return res.json({"error": "Email does not match"})
+    }
+
+    if (password !== cpassword) {
+        res.status(400);
+        return res.json({"error": "Password does not match"})
+    }
+
+    try {
+        await pool.query(
+            `INSERT INTO users(username, email, password_hash)
+             VALUES ($1, $2, crypt($3, gen_salt('md5')))
+             RETURNING *`,
+            [username, email, password],
+        ).then(() => {
+            return res.sendStatus(200);
+        })
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(error.status);
+    }
 };
 
 exports.updateUser = (req, res) => {
-    const { id } = req.params;
+    const {id} = req.params;
     res.send(`User with ID: ${id} updated`);
 };
 
 exports.deleteUser = (req, res) => {
-    const { id } = req.params;
+    const {id} = req.params;
     res.send(`User with ID: ${id} deleted`);
 };
