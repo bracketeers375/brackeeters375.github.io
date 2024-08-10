080603f9438112313f9891188c3239b966b61792
Steven <sd3522@drexel.edu>
2024-08-10 12:38:47 -0400

    some comment to signify

--- src/app/server.js
+++ src/app/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import routes from "./routes";
+import * as routes from "./routes/";
 const app = express();
 let hostname = "0.0.0.0";
 let port = 3000;
@@ -7,7 +7,10 @@ let port = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+//THIS COMMENTED LINE NEEDS TO BE CONVERTED TO ESM
+//app.use("/api", require("./routes"));
 app.use("/api", routes);
+
 
 app.listen(port, hostname, () => {
   console.log(`http://${hostname}:${port}`);
