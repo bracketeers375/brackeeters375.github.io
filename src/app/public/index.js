e8432cbf5313c9e02a524f822049327871effc8c
ehn24 <ehn24@drexel.edu>
2024-08-10 01:41:28 -0400

    Search Index WIP

---
+++ src/app/public/index.js
@@ -0,0 +1,48 @@
+const discoverLink = document.getElementById("discover-link");
+const languageLink = document.getElementById("language-link");
+const loginLink = document.getElementById("login-link");
+const searchLink = document.getElementById("search-link");
+const secondNav = document.querySelector(".second-sidenav");
+
+function hideSecondNav() {
+  secondNav.style.display = "none";
+}
+
+function showSecondNav(content) {
+  secondNav.style.display = "block";
+  secondNav.textContent = "";
+  secondNav.appendChild(content);
+}
+
+searchLink.addEventListener("click", () => {
+  window.location.href = 'search.html';
+});
+
+
+discoverLink.addEventListener("click", () => {
+  const panelTextDiscover = document.createElement("a");
+  panelTextDiscover.textContent = "Discover Test";
+  panelTextDiscover.href = "#";
+  showSecondNav(panelTextDiscover);
+});
+
+languageLink.addEventListener("click", () => {
+  const panelTextLanguage = document.createElement("a");
+  panelTextLanguage.textContent = "Language Test (WIP)";
+  panelTextLanguage.href = "#";
+  showSecondNav(panelTextLanguage);
+});
+
+loginLink.addEventListener("click", () => {
+  const panelTextLogin = document.createElement("a");
+  panelTextLogin.textContent = "Login Test";
+  panelTextLogin.href = "#";
+  showSecondNav(panelTextLogin);
+});
+
+document.addEventListener("click", (event) => {
+  if (!secondNav.contains(event.target) && !event.target.closest(".sidenav")) {
+    hideSecondNav();
+  }
+});
+
