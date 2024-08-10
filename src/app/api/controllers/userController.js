43406428414751178dd5d56b19369534157c715a
lm-rice <lrice1116@gmail.com>
2024-08-09 22:36:34 -0400

    Separate Postgres connection bootstrapping to another file

--- src/app/api/controllers/userController.js
+++ src/app/api/controllers/userController.js
@@ -1,10 +1,4 @@
-const { Pool } = require("pg");
-const pool = new Pool({
-  connectionString: process.env.APP_CONNECTION_STRING,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
+const { pool } = require('../../connection');
 
 exports.getUserById = async (req, res) => {
   const { id } = req.params;
