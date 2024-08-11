a41718e649e73a0dd8ff922024c7abc66617e19b
Steven <sd3522@drexel.edu>
2024-08-10 20:11:16 -0400

    prettier

--- src/app/connection.js
+++ src/app/connection.js
@@ -4,14 +4,15 @@ const { Pool } = pkg;
 let databaseConfig;
 // fly.io sets NODE_ENV to production automatically, otherwise it's unset when running locally
 if (process.env.NODE_ENV == "production") {
-	databaseConfig = { connectionString: process.env.DATABASE_URL };
+  databaseConfig = { connectionString: process.env.DATABASE_URL };
 } else {
-	databaseConfig = {
-                                "user": "postgres",
-                                "password": "astronautHyper6Zhou",
-                                "host": "localhost",
-                                "database": "local_tournament_db",
-                                "port": 5432 };
+  databaseConfig = {
+    user: "postgres",
+    password: "astronautHyper6Zhou",
+    host: "localhost",
+    database: "local_tournament_db",
+    port: 5432,
+  };
 }
 
 const pool = new Pool(databaseConfig);
