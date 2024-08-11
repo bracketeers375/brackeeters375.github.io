f0419d975944f8d669cb18b7ca5f4a16729db742
ehn24 <ehn24@drexel.edu>
2024-08-10 22:58:56 -0400

    prettier run

--- src/app/api/controllers/searchController.js
+++ src/app/api/controllers/searchController.js
@@ -1,9 +1,13 @@
 import express from "express";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import searchService from "../services/searchService.js";
 =======
 import participantsService from '../services/searchService.js';
 >>>>>>> aaffe15 (organized/routed search)
+=======
+import participantsService from "../services/searchService.js";
+>>>>>>> afe8be9 (prettier run)
 
 const searchTournamentsByName = async (req, res) => {
   const name = req.query.name;
@@ -11,6 +15,7 @@ const searchTournamentsByName = async (req, res) => {
   try {
     const result = await searchService.getTournamentByName(name);
     let body = {
+<<<<<<< HEAD
 <<<<<<< HEAD
       tourneys: results,
     };
@@ -21,6 +26,11 @@ const searchTournamentsByName = async (req, res) => {
     return res.json(body);
 
 >>>>>>> aaffe15 (organized/routed search)
+=======
+      tourneys: results,
+    };
+    return res.json(body);
+>>>>>>> afe8be9 (prettier run)
   } catch (error) {
     console.log(error);
     res.status(500).send("An error occurred while searching for tournaments.");
@@ -29,9 +39,13 @@ const searchTournamentsByName = async (req, res) => {
 
 const searchRouter = express.Router();
 <<<<<<< HEAD
+<<<<<<< HEAD
 searchRouter.get("/getAll/:name", getTournamentByName);
 =======
 searchRouter.get("/getAll/:name", getTournamentByName)
 >>>>>>> aaffe15 (organized/routed search)
+=======
+searchRouter.get("/getAll/:name", getTournamentByName);
+>>>>>>> afe8be9 (prettier run)
 
 export default searchRouter;
--- src/app/api/services/searchService.js
+++ src/app/api/services/searchService.js
@@ -26,24 +26,30 @@ export default {
 import pool from "../../connection.js";
 
 const getTournamentsByName = async (name) => {
-	try {
-		const result = await pool.query(
-			`SELECT * FROM SELECT * FROM tournaments WHERE name ILIKE $1`, [name]
-			);
+  try {
+    const result = await pool.query(
+      `SELECT * FROM SELECT * FROM tournaments WHERE name ILIKE $1`,
+      [name],
+    );
 
-		if (result.rows.length === 0) {
-			return null;
-		}
+    if (result.rows.length === 0) {
+      return null;
+    }
 
-		return result.rows;
-	} catch (error) {
-		console.log(error);
-		throw new Error("Database error");
-	}
+    return result.rows;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Database error");
+  }
 };
 
 export default {
+<<<<<<< HEAD
 	getTournamentsByName
 }
 
 >>>>>>> aaffe15 (organized/routed search)
+=======
+  getTournamentsByName,
+};
+>>>>>>> afe8be9 (prettier run)
