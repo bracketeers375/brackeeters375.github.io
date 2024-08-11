b4134b24083e7b35cffaf7f8fcc1c2192251a9ea
Steven <sd3522@drexel.edu>
2024-08-10 20:08:11 -0400

    basic attendees search POC

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -3,8 +3,10 @@ const apiRouter = express.Router();
 
 import userRouter from "./controllers/userController.js";
 import tournamentRouter from "./controllers/tournamentController.js";
+import participantRouter from "./controllers/participantsController.js";
 
 apiRouter.use("/users", userRouter);
 apiRouter.use("/tournaments", tournamentRouter);
+apiRouter.use("/participants", participantRouter);
 
 export default apiRouter;
---
+++ src/app/api/services/participantsService.js
@@ -0,0 +1,24 @@
+import pool from "../../connection.js";
+
+const getParticipantsByTourId = async (tournament_id) => {
+    try {
+        const result = await pool.query(
+            `SELECT * FROM Participants
+            WHERE tournament_id =$1`,
+            [tournament_id]
+        );
+
+        if (result.rows.length === 0) {
+            return null; // Return null if no participants is found
+        }
+
+        return result.rows;
+    } catch (error) {
+        console.log(error);
+        throw new Error("Database error");
+    }
+};
+
+export default {
+    getParticipantsByTourId
+}
