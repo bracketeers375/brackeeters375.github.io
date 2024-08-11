733b5dc7599ab6f225d5e218137133484ad67607
ehn24 <ehn24@drexel.edu>
2024-08-10 20:19:31 -0400

    updated search

--- src/app/public/search.js
+++ src/app/public/search.js
@@ -1,12 +1,27 @@
-document.getElementById('search-button').addEventListener('click', function() {
-  const name = document.getElementById('search-input').value;
+document.getElementById("search-button").addEventListener("click", function () {
+  const name = document.getElementById("search-input").value;
 
-  fetch(`http://localhost:3000/search?name=${encodeURIComponent(name)}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
+  fetch(`/search?name=${name}`)
+    .then((response) => {
+      if (response.headers.get("content-type").includes("application/json")) {
+        return response.json();
+      } else {
+        return response.text(); // Or throw an error
+      }
     })
-    .catch(error => {
-      console.error('Fetch error:', error);
+    .then((data) => {
+      const searchResultsContainer = document.getElementById("search-results");
+      console.log(data);
+      data.forEach((tournament) => {
+        const tournamentElement = document.createElement("div");
+        tournamentElement.textContent = `Tournament: ${tournament.name}`;
+        searchResultsContainer.appendChild(tournamentElement);
+      });
+    })
+    .catch((error) => {
+      const searchResultsContainer = document.getElementById("search-results");
+      const errorMessageElement = document.createElement("div");
+      errorMessageElement.textContent = `Error getting results.`;
+      searchResultsContainer.appendChild(errorMessageElement);
     });
 });
