afe8be99d892c9ba3f5949fc9b25b35954fb33cd
ehn24 <ehn24@drexel.edu>
2024-08-10 22:58:56 -0400

    prettier run

--- src/app/api/services/searchService.js
+++ src/app/api/services/searchService.js
@@ -1,23 +1,23 @@
 import pool from "../../connection.js";
 
 const getTournamentsByName = async (name) => {
-	try {
-		const result = await pool.query(
-			`SELECT * FROM SELECT * FROM tournaments WHERE name ILIKE $1`, [name]
-			);
+  try {
+    const result = await pool.query(
+      `SELECT * FROM SELECT * FROM tournaments WHERE name ILIKE $1`,
+      [name],
+    );
 
-		if (result.rows.length === 0) {
-			return null;
-		}
+    if (result.rows.length === 0) {
+      return null;
+    }
 
-		return result.rows;
-	} catch (error) {
-		console.log(error);
-		throw new Error("Database error");
-	}
+    return result.rows;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Database error");
+  }
 };
 
 export default {
-	getTournamentsByName
-}
-
+  getTournamentsByName,
+};
