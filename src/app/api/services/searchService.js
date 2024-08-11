12eb677b6e76b86270c7680993eb3620796cb602
ehn24 <ehn24@drexel.edu>
2024-08-10 21:37:50 -0400

    organized/routed search

--- src/app/api/services/searchService.js
+++ src/app/api/services/searchService.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pool from "./connection.js";
 
 const getTournamentsByName = async (name) => {
@@ -21,3 +22,28 @@ const getTournamentsByName = async (name) => {
 export default {
   getTournamentsByName,
 };
+=======
+import pool from "../../connection.js";
+
+const getTournamentsByName = async (name) => {
+	try {
+		const result = await pool.query(
+			`SELECT * FROM SELECT * FROM tournaments WHERE name ILIKE $1`, [name]
+			);
+
+		if (result.rows.length === 0) {
+			return null;
+		}
+
+		return result.rows;
+	} catch (error) {
+		console.log(error);
+		throw new Error("Database error");
+	}
+};
+
+export default {
+	getTournamentsByName
+}
+
+>>>>>>> aaffe15 (organized/routed search)
