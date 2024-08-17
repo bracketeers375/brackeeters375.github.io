fbeeb53a691deddd45e1e1fa63e6e092e4183ed1
Steven <sd3522@drexel.edu>
2024-08-17 14:42:41 -0400

    Example bracket rendered from a form. Not exactly working

--- src/app/api/controllers/bracketsController.js
+++ src/app/api/controllers/bracketsController.js
@@ -1,22 +1,33 @@
 import express from "express";
 import bracketsService from "../services/bracketsService.js";
 
-const createBracket = async (req, res) => {
+const createBracketTest = async (req, res) => {
+    console.log("test 123")
     const { numPlayers, format } = req.body;
     
     if(!numPlayers || !format) {
         return res.status(400).send("Missing required fields");
     }
 
+    try {
+        const tournamentJsonData = await bracketsService.createBracketTest(numPlayers, format);
+        let body = {
+            tournamentData: tournamentJsonData,
+        };
+        return res.json(body);
+    }   catch (error) {
+        res.status(500);
+        res.send("An error occured while creating bracket");
+    }
 
 };
 
 const bracketsRouter = express.Router();
 
-bracketsRouter.get("/get/:id", getBracketById);
-bracketsRouter.post("/create", createBracket);
-bracketsRouter.put("/update/:id", updateBracket);
-bracketsRouter.delete("/delete/:id", deleteBracket);
+// bracketsRouter.get("/get/:id", getBracketById);
+bracketsRouter.post("/createTest", createBracketTest);
+// bracketsRouter.put("/update/:id", updateBracket);
+// bracketsRouter.delete("/delete/:id", deleteBracket);
 
 
 export default bracketsRouter;
