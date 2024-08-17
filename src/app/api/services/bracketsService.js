eca97ed8b043c8e6eef30df9ce24d65644126d89
Steven <sd3522@drexel.edu>
2024-08-17 14:42:41 -0400

    Example bracket rendered from a form. Not exactly working

--- src/app/api/services/bracketsService.js
+++ src/app/api/services/bracketsService.js
@@ -1,3 +1,37 @@
 import pool from "./connection.js";
-import {InMemoryDatabase} from "brackets-memory-db";
+import { InMemoryDatabase } from "brackets-memory-db";
 import { BracketsManager } from "brackets-manager";
+import { json } from "express";
+
+const storage = new InMemoryDatabase();
+const manager = new BracketsManager(storage);
+
+const createBracketTest = async (numPlayers, format) => {
+    console.log("createBracketTestServce");
+    console.log("numPlayers: ", numPlayers);
+    let seedinglist = [];
+
+    for(let i = 0; i < numPlayers; i++) {
+        let playerName = "Player " + i;
+        seedinglist.push(playerName);
+    }
+
+    console.log("seedinglist: ", seedinglist);
+    await manager.create.stage({
+        tournamentId: "123",
+        type: format,
+        name: "Example stage",
+        seeding: seedinglist,
+        settings: {grandFinal: 'double' },
+    });
+
+    let jsonData = await manager.export();
+    console.log("jsonData: ", jsonData);
+
+    return jsonData;
+
+}
+
+export default {
+    createBracketTest,
+};
