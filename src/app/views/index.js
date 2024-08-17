715c7241d2f43ebff76c03a8d9c993a5b56c0ff8
lm-rice <lrice1116@gmail.com>
2024-08-17 18:12:33 -0400

    Update UI: rework sidebar, add login popup modal, introduce reusable partials

--- src/app/views/index.js
+++ src/app/views/index.js
@@ -2,32 +2,38 @@ import express from "express";
 const viewRouter = express.Router();
 
 viewRouter.get("/", (req, res) => {
-  res.render(`./index`, {
+  res.render(`index`, {
     title: 'finish.gg - Community through competition'
   });
 });
 
 viewRouter.get("/login", (req, res) => {
-  res.render(`./login`, {
+  res.render(`login`, {
     title: "Login - Bracketeers",
   });
 });
 
 viewRouter.get("/registration", (req, res) => {
-  res.render(`./registration`, {
+  res.render(`registration`, {
     title: "Registration - Bracketeers",
   });
 });
 
 viewRouter.get("/search", (req, res) => {
-  res.render(`./search`, {
+  res.render(`search`, {
     title: "Search - Bracketeers",
   });
 });
 
 viewRouter.get("/attendees", (req, res) => {
-  res.render(`./attendees`, {
+  res.render(`attendees`, {
     title: "Attendees List - Bracketeers",
+  });
+});
+
+viewRouter.use((req, res) => {
+  res.status(404).render('404', {
+    title: '404 - Page Not Found'
   });
 });
 
