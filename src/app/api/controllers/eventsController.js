2940a86fa4933b0e8713351905a4f2e7bb40de9e
ehn77 <99348876+EthanNguyenCoding@users.noreply.github.com>
2024-08-23 16:26:33 -0400

    Feature/events (#30)
    * events edit
    
    * added router + fixed syntax

--- src/app/api/controllers/eventsController.js
+++ src/app/api/controllers/eventsController.js
@@ -31,9 +31,57 @@ const getAllEventsByTournamentId = async (req, res) => {
   }
 };
 
+const getDetails = async (req, res) => {
+  let { eventName } = req.body;
+  if (!eventName) {
+    return res.status(400).send("Missing parameter");
+  }
+
+  try {
+    await eventsService.createEvent(eventName);
+    res.status(200).send("Event created successfully");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
+};
+
+const updateDetails = async (req, res) => {
+  let { eventName, updatedEventName } = req.body;
+  if (!eventName || !updatedEventName) {
+    return res.status(400).send("Missing parameters");
+  }
+
+  try {
+    await eventsService.updateEvent(eventName, updatedEventName);
+    res.status(200).send("Event updated successfully");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
+};
+
+const deleteDetails = async (req, res) => {
+  let { deleteEvent } = req.body;
+  if (!deleteEvent) {
+    return res.status(400).send("Missing parameter");
+  }
+
+  try {
+    await eventsService.deleteEvent(deleteEvent);
+    res.status(200).send("Event deleted successfully");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
+};
+
 const eventRouter = express.Router();
 
 eventRouter.get("/get/:id", getEventById);
 eventRouter.get("/getAll/:id", getAllEventsByTournamentId);
+eventRouter.delete("/delete", deleteDetails);
+eventRouter.post("/update", updateDetails);
+eventRouter.post("/create", getDetails);
 
 export default eventRouter;
